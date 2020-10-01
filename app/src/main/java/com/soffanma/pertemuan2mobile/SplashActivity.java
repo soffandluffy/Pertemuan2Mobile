2a55f35456511fa68d4094bf76f995098f5ca1e7
Soffan Marsus Ahmad <soffanma@gmail.com>
2020-10-01 09:47:36 +0700

    Add splash screen

---
+++ app/src/main/java/com/soffanma/pertemuan2mobile/SplashActivity.java
@@ -0,0 +1,32 @@
+package com.soffanma.pertemuan2mobile;
+
+import android.annotation.SuppressLint;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.MotionEvent;
+import android.view.View;
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Intent main = new Intent(SplashActivity.this, StyleActivity.class);
+                startActivity(main);
+                finish();
+
+            }
+        },4000);
+
+    }
+}
